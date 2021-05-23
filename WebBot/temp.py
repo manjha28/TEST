600e45f3c89de5c089d1f7dd46a3fdd342b84f96
Manish Jha <58687464+manjha28@users.noreply.github.com>
2021-05-24 00:25:25 +0530

    Update : Implementing Explicit wait

--- WebBot/temp.py
+++ WebBot/temp.py
@@ -1,5 +1,16 @@
+'''
+Author : Manish Jha
+Date : 20210524
+'''
+
+
 import os
 from selenium import webdriver
+
+'''These are the imports required for implementing Explicit wait'''
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import logging
 # os.environ['PATH'] += "<Driver Folder path>"
 logging.basicConfig(level=logging.INFO)
@@ -20,3 +31,16 @@ driver.implicitly_wait(5)
 
 download = driver.find_element_by_id('downloadButton')
 download.click()
+
+'''This aproach won't work here, need to go with explicit wait'''
+# progress = driver.find_element_by_class_name('progress-label')
+# driver.implicitly_wait(30)
+# print(f"{progress.text}")
+
+WebDriverWait(driver, 30).until(
+    EC.text_to_be_present_in_element(
+        (By.CLASS_NAME, 'progress-label'),
+        'Complete!'
+    )
+)
+logger.info(f"The text is matched")
